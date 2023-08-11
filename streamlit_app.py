c71b02d25c07e7bd67cdd60b0c24cda5a26f8f49
Andy Sanderson <77621960+andysanderson@users.noreply.github.com>
2023-08-11 15:52:31 +0100

    text box to pick the fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,9 +22,12 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-
 streamlit.header("Fruityvice Fruit Advice!")
+
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # normalize the data
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
