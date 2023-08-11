a5af19e34475a2cdaa06fd5038ba36b435246be2
Andy Sanderson <77621960+andysanderson@users.noreply.github.com>
2023-08-11 18:18:26 +0100

    add function to add real fruits back to SNOW

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,6 +51,11 @@ def get_fruit_load_list():
     my_cur.execute("SELECT * from fruit_load_list")
     return my_cur.fetchall()
 
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("INSERT into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('" + new_fruit + "')")
+    return "Thanks for adding " + new_fruit
+    
 # Button to load the fruit
 if streamlit.button('Get Fruit Load List'):
   # Connect to SNOW
@@ -59,11 +64,10 @@ if streamlit.button('Get Fruit Load List'):
   streamlit.dataframe(my_data_rows)
 
 
-
-
 # Allow users to add more fruits
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
-streamlit.write('Thanks for adding ', add_my_fruit)
-
-# This is for testing
-#my_cur.execute("INSERT into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('from streamlit')")
+if streamlit.button('Add a Fruit to the List'):
+  # Connect to SNOW
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_function = insert_row_snowflake(add_my_fruit)
+  streamlit.text(back_from_function)
